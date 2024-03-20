f03be9eacacf57a7970693d31cae39c5e5be4f8f
bastiangraver <bastian.ven@gmail.com>
2024-03-20 14:32:11 +0100

    Docker setup, Database mounted and login functional. Testing mangler

--- user/app.py
+++ user/app.py
@@ -2,38 +2,67 @@ from flask import Flask, request, render_template, redirect, url_for, jsonify, s
 from flask_bootstrap import Bootstrap  # Import Flask-Bootstrap
 from werkzeug.utils import secure_filename
 import os
+from flask_sqlalchemy import *
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)  # Initialize Flask-Bootstrap
 app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024  # for 64 MB limit
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
 app.secret_key = 'Jacobo'  # Set a secret key for session management
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+    
 
 
 assignments = [("Assignment 1", 0), ("Assignment 2", 1), ("Assignment 3", 3), ("Assignment 4", 4), ("Assignment 5", 2)]
 @app.route("/student")
-def index():
-    # Ensure assignments is defined (even if empty) before sorting and passing to the template
-    username = session.get('username', 'No User')  
-    
-    if assignments is None:
-        sorted_assignments = []
-    else:
-        sorted_assignments = sorted(assignments, key=lambda x: x[1])
+def student():
+    if session:
+        # Ensure assignments is defined (even if empty) before sorting and passing to the template
+        username = session.get('username', 'No User')  
+        
+        if assignments is None:
+            sorted_assignments = []
+        else:
+            sorted_assignments = sorted(assignments, key=lambda x: x[1])
 
-    return render_template("index.html", nav= f"Dashbord for: {username}", assignments=sorted_assignments)
+        return render_template("student.html", nav= f"Dashbord for: {username}", assignments=sorted_assignments)
+    else:
+        return redirect(url_for('login', error="You need to login first"))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == "admin" and password == "admin":
-            session['username'] = username 
-            return redirect(url_for('index'))
+        if username and password: # Check if the inputs are not empty
+            student = Student.query.filter_by(username=username).first()
+            # Check if the student exists and the username and the password is correct
+            if student and username == student.username and password == student.password: # Check if the username and password are correct
+                session['username'] = student.username 
+                return redirect(url_for('student'))
+            else:
+                return render_template('login.html', nav="Login", error="Invalid username or password")
         else:
-            return render_template('login.html', nav="Login", error="Invalid username or password")
+            return render_template('login.html', nav="Login", error="Input cannot be empty")
     else:
         return render_template('login.html', nav="Login")
+
+@app.route('/logout')
+def logout():
+    # Clear the session
+    session.clear()
+    # Redirect to the login page
+    return redirect(url_for('login'))
 
 
 @app.route('/student/submit', methods=['GET', 'POST'])
