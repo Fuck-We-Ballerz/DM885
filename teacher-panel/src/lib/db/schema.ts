29abd419108498cd3501b57254c921cf4ba63d03
William <william.kramhyle@gmail.com>
2024-04-15 18:00:28 +0200

    Added our local definition of the schema.

--- teacher-panel/src/lib/db/schema.ts
+++ teacher-panel/src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, foreignKey, integer, pgTable, primaryKey, serial, timestamp, unique, varchar } from 'drizzle-orm/pg-core';
+import { boolean, foreignKey, integer, pgTable, primaryKey, serial, text, timestamp, unique, varchar } from 'drizzle-orm/pg-core';
 export const assignmentConfig = pgTable('assignment_config', {
   id: serial('id').primaryKey(),
   max_ram: integer('max_ram').notNull(),
@@ -9,6 +9,7 @@ export const assignmentConfig = pgTable('assignment_config', {
 
 export const assignment = pgTable('assignment', {
   id: serial('id').primaryKey(),
+  title: varchar('title', {length: 64}).notNull(),
   start_date: timestamp('start_date', { mode: 'string' }).notNull().defaultNow(),
   end_date: timestamp('end_date', { mode: 'string' }).notNull().defaultNow(),
   is_visible: boolean('is_visible').notNull(),
@@ -51,6 +52,24 @@ export const submission = pgTable('submission', {
     assignment_id: integer('assignment_id').notNull().references(() => assignment.id),
     student_id: integer('student_id').notNull().references(() => student.id),
 });
+
+export const course = pgTable('course', {
+    id: serial('id').primaryKey(),
+    name: text('name')
+});
+
+
+export const student_to_course = pgTable('student_to_course', {
+    id: serial('id').primaryKey(),
+    student_id: integer('student_id').notNull().references(() => student.id),
+    course_id: integer('course_id').notNull().references(() => course.id),
+})
+
+export const teacher_to_course = pgTable('student_to_course', {
+    id: serial('id').primaryKey(),
+    teacher_id: integer('teacher_id').notNull().references(() => teacher.id),
+    course_id: integer('course_id').notNull().references(() => course.id),
+})
 
 export const studentToAssignment = pgTable('student_to_assignment', {
     assignment_id: integer('assignment_id').primaryKey().references(() => assignment.id),
