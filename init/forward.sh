2a7978a5d7e721650c1b48027a3f3bb574a1732c
= <=>
2024-04-21 20:18:17 +0200

    Formatted, changed ports, and cleaning

--- init/forward.sh
+++ init/forward.sh
@@ -1,3 +1,3 @@
-kubectl port-forward service/keycloak 3200:3200 &
-kubectl port-forward service/adminer 3300:3300 &
+kubectl port-forward service/keycloak 3200:8080 &
+kubectl port-forward service/adminer 3300:8080 &
 kubectl port-forward service/grafana 3000:3000
