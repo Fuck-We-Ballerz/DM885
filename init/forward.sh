3b345848825207d697ad090012f4039a68151173
= <=>
2024-04-22 18:50:44 +0200

    new port forward

--- init/forward.sh
+++ init/forward.sh
@@ -3,4 +3,4 @@ kubectl port-forward service/adminer 3300:8080 &
 kubectl port-forward service/grafana 3000:3000 &
 kubectl port-forward service/prometheus 9090:9090 &
 kubectl port-forward service/postgres-exporter 9187:9187 &
-
+kubectl port-forward service/promtail 9080:9080
