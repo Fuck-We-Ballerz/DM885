c44cfc012f14dbbaee3f63e357b31e1a8932c649
= <=>
2024-04-22 16:47:24 +0200

    Fixed

--- init/forward.sh
+++ init/forward.sh
@@ -1,3 +1,4 @@
 kubectl port-forward service/keycloak 3200:8080 &
 kubectl port-forward service/adminer 3300:8080 &
-kubectl port-forward service/grafana 3000:3000
+kubectl port-forward service/grafana 3000:3000 &
+kubectl port-forward service/prometheus 9090:9090
