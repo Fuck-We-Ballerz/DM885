61369e7bc6f2d1dc580e60c492e1ab99e01d3b9f
= <=>
2024-04-22 18:01:51 +0200

    Update

--- init/forward.sh
+++ init/forward.sh
@@ -1,4 +1,6 @@
 kubectl port-forward service/keycloak 3200:8080 &
 kubectl port-forward service/adminer 3300:8080 &
 kubectl port-forward service/grafana 3000:3000 &
-kubectl port-forward service/prometheus 9090:9090
+kubectl port-forward service/prometheus 9090:9090 &
+kubectl port-forward service/postgres-exporter 9187:9187 &
+
