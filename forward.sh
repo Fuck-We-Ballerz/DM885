ad6160e408e192fc825942d8e566b0887da1196c
Tobias Svane Mikkelsen <tobiassm12@hotmail.dk>
2024-05-14 18:20:51 +0200

    helm

--- forward.sh
+++
@@ -1,9 +0,0 @@
-kubectl port-forward service/keycloak 3200:8080 &
-kubectl port-forward service/adminer 3300:8080 &
-kubectl port-forward service/grafana 3000:3000 &
-kubectl port-forward service/prometheus 9090:9090 &
-kubectl port-forward service/postgres-exporter 9187:9187 &
-kubectl port-forward service/promtail 9080:9080 &
-kubectl port-forward service/teacher-service 5173:5173 &
-kubectl port-forward service/student-service 5050:5050 &
-kubectl port-forward service/kube-state-metrics 8080:8080
