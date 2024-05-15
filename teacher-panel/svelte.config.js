861e08e59407824a8be584fb35e4ab5f2c4a55ee
Nikolaj Dall <nidal20@student.sdu.dk>
2024-05-15 12:53:32 +0200

    no base url for now

--- teacher-panel/svelte.config.js
+++ teacher-panel/svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: '/teacher',
+			// base: '/teacher',
+			base: '',
 		},
 	}
 };
