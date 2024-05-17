da904180c5ede954958c88db134310a13f869e55
ZerusTS <paulschwarz9@gmail.com>
2024-05-17 15:03:59 +0200

    changed svelt env import

--- frontend/teacher-panel/src/routes/+page.svelte
+++ frontend/teacher-panel/src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script>
-    import { PUBLIC_STUDENT_LOGIN_URL } from "$env/static/public";
     import { base } from '$app/paths';
-
 </script>
 
 <h1>Choose your role below</h1>
 
-<button on:click={() => { window.location.href = `${PUBLIC_STUDENT_LOGIN_URL}`; }}>Student</button>
+<button on:click={() => { window.location.href = process.env.PUBLIC_STUDENT_LOGIN_URL; }}>Student</button>
 <button on:click={() => { window.location.href = `${base}/teacher`; }}>Teacher</button>
 <button on:click={() => { window.location.href = `${base}/admin`; }}>Admin</button>
 
